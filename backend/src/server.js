bbfaecc5bdd18f36cf7cf7ffceb843d3c447e071
Prem Kumar Pathak <nsprem114@gmail.com>
2026-02-09 01:58:55 +0530

    prepare for the development

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,29 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config({ path: "./src/.env" });
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 
+if (process.env.NOTD_ENV !== "production") {
+  app.use(cors());
+}
+
 app.use("/api/notes", noteRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../forntend/dist")));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../forntend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
